cfac5057037789b3eafd82b1f073ef80b0d9b4a6
manohitha416 <manohitha9@gmail.com>
2025-07-08 01:54:06 -0400

    first commit

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test
+{
+    public static void main(String args[])
+    {
+        System.out.println("jenkins test");
+        System.out.println("check");
+        System.out.println("diff");
+    }
+}
